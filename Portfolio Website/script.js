70b9e5bf0ad6e6f6584b363a02a676b51b691a1f
Olaoye Salem <89555234+olaoyesalem@users.noreply.github.com>
2023-07-05 14:41:46 +0000

    Update script.js

--- Portfolio Website/script.js
+++ Portfolio Website/script.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
         
      
     
-    
         // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
